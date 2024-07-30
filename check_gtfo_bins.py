c9e85b021814c66c53506011b0c04bb2fc976179
eakeyson1 <64662279+eakeyson1@users.noreply.github.com>
2024-07-30 10:05:42 -0600

    Update check_gtfo_bins.py
    Updating TODO

--- check_gtfo_bins.py
+++ check_gtfo_bins.py
@@ -21,3 +21,13 @@ for i in list_of_cmds:
 	r = requests.get(URL)
 	if(r.status_code == 200):
 		print(i + " commmand exist")
+
+
+# Adding functionality that compares normal output of the find command to see if there are any custom scripts with SGID or SUID bit set
+
+# find / -perm -u=s -type f 2>/dev/null
+# 4000 is the bitmask for SetUID
+
+
+# find / -perm /4000 2>/dev/null
+# 2000 is the bitmask for SetGID
